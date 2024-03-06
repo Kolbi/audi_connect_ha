29790cb0e08a1755229284253a1842a6fc73b206
Kolbi <MatthiasKolb@2-wheel-planet.de>
2024-03-06 08:05:35 +0100

    Update device_tracker.py

--- custom_components/audiconnect/device_tracker.py
+++ custom_components/audiconnect/device_tracker.py
@@ -1,7 +1,7 @@
 """Support for tracking an Audi."""
 import logging
 
-from homeassistant.components.device_tracker import SOURCE_TYPE_GPS
+from homeassistant.components.device_tracker import SourceType.GPS
 from homeassistant.helpers.dispatcher import (
     async_dispatcher_connect,
     async_dispatcher_send,
