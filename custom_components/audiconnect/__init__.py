27a818e035e9d0d0cf3f131dc34af7d271dbf35b
Kolbi <MatthiasKolb@2-wheel-planet.de>
2024-03-12 16:46:40 +0100

    Update __init__.py

--- custom_components/audiconnect/__init__.py
+++ custom_components/audiconnect/__init__.py
@@ -123,3 +123,8 @@ async def async_unload_entry(hass, config_entry):
     del hass.data[DOMAIN][account]
 
     return True
+
+async def async_remove_config_entry_device(
+    hass: HomeAssistant, config_entry: ConfigEntry, device_entry: DeviceEntry
+) -> bool:
+    """Remove a config entry from a device."""
