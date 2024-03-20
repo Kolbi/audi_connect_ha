7dc6e9e100f6c3d98aed30f8af750dc117fa5486
Kolbi <MatthiasKolb@2-wheel-planet.de>
2024-03-20 11:11:19 +0100

    Update dashboard.py

--- custom_components/audiconnect/dashboard.py
+++ custom_components/audiconnect/dashboard.py
@@ -361,6 +361,7 @@ class LastUpdate(Instrument):
             name="Last Update",
             icon="mdi:update",
         )
+        self.device_class = "timestamp"
         self.unit = None
 
     @property
