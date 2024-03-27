847628406785d8d45af037b8cd7c4d8ec9204c71
Kolbi <MatthiasKolb@2-wheel-planet.de>
2024-03-27 14:11:04 +0100

    Update __init__.py

--- custom_components/audiconnect/__init__.py
+++ custom_components/audiconnect/__init__.py
@@ -131,7 +131,7 @@ async def async_unload_entry(hass, config_entry):
     return True
 
 
-async def async_migrate_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
+async def async_migrate_entry(self, hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     """Migrates an old configuration entry to version 2."""
 
     if config_entry.version == 1:
