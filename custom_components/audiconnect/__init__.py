a54ec47f0717113c214848eab5e1fc23b60df36e
Kolbi <MatthiasKolb@2-wheel-planet.de>
2024-04-02 11:48:16 +0200

    Update __init__.py

--- custom_components/audiconnect/__init__.py
+++ custom_components/audiconnect/__init__.py
@@ -149,12 +149,6 @@ async def async_migrate_entry(
         # Get device registry and iterate through devices
         device_registry = dr.async_get(hass)
         for entry_id, device in device_registry.devices.items():
-            # if device.domain == DOMAIN and "identifiers" in device.config_entries:
-            old_identifier = None
-            for identifier in device.config_entries["identifiers"]:
-
-            # Check for old identifier using f-string for clarity
-            if old_identifier[1] == self._instrument.vehicle_name:
                 _LOGGER.info(
                     f"Migrating device {device.name} ({device.id}) to new identifier"
                 )
