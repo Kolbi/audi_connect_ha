5aba973cc8ed9bdcd77c9a14ce34a00e376ed4e7
Kolbi <MatthiasKolb@2-wheel-planet.de>
2024-04-05 11:36:14 +0200

    Update binary_sensor.py

--- custom_components/audiconnect/binary_sensor.py
+++ custom_components/audiconnect/binary_sensor.py
@@ -41,3 +41,8 @@ class AudiSensor(AudiEntity, BinarySensorEntity):
         if self._instrument.device_class in DEVICE_CLASSES:
             return self._instrument.device_class
         return None
+
+  @property
+    def state_class(self):
+        """Return the state_class"""
+        return self._instrument.state_class
